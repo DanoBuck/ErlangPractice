c41c6abddbc80488ae42d466349aedb22889d491
DanoBuck <daniielbuckleyty3@gmail.com>
2017-07-29 22:37:11 +0100

    Summing list up recursively

--- numbers.erl
+++ numbers.erl
@@ -40,6 +40,22 @@ findMinimumRecursiveHelper(Min, Next, List) ->
 builtInMinimumItemInList(List) ->
   lists:min(List).
 
+sumList(List) ->
+  sumList(List, 0).
+
+sumList(List, Sum) ->
+%%  Types are immutable so don't forget to create a new variable when trying to do some computation
+  if length(List) == 1 ->
+    NewSum = Sum + lists:nth(1, List);
+    length(List) > 1 ->
+      NewSum = Sum + lists:nth(1, List),
+      NewList = lists:delete(lists:nth(1, List), List),
+      sumList(NewList, NewSum)
+  end.
+
+builtInSumOfList(List) ->
+  lists:sum(List).
+
 main() ->
   List = [15,12,100,1200,12,1,4,5,17,15,11],
 %%  erlang:display(List),
@@ -49,5 +65,12 @@ main() ->
 
   Min2 = builtInMinimumItemInList(List),
   io:format("~s", ["The minimum value in list using built in function is "]),
-  erlang:display(Min2).
+  erlang:display(Min2),
+
+  io:format("~s", ["\nThe added value in list using recursive function is "]),
+  erlang:display(sumList(List)),
+
+  io:format("~s", ["The added value in list using built in function is "]),
+  erlang:display(builtInSumOfList(List))
+.
 
